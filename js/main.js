aefe046ad277b33b229a74ec2b4a078cd5592e78
Evgenii Popov <jekson87@me.com>
2020-01-23 21:47:19 +0300

    disable re-rendering

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World!'
+        title: 'Hello World!',
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">Google</a>'
     },
     methods: {
         sayHello: function() {
+            this.title = 'Hello!';
             return this.title;
         }
     }
