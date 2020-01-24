e864d25ec041a00e1a0102464719758900d3984d
Evgenii Popov <jekson87@me.com>
2020-01-25 00:00:26 +0300

    modifying an event

--- js/main.js
+++ js/main.js
@@ -4,15 +4,24 @@ new Vue({
         title: 'Hello World!',
         link: 'http://google.com',
         finishedLink: '<a href="http://google.com">Google</a>',
-        counter: 0
+        counter: 0,
+        x: 0,
+        y: 0
     },
     methods: {
         sayHello: function() {
             this.title = 'Hello!';
             return this.title;
         },
-        increase: function() {
-            this.counter++;
+        increase: function(step, event) {
+            this.counter += step;;
+        },
+        updateCoordinates: function(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        dummy: function(event) {
+            event.stopPropagation();
         }
     }
 });
