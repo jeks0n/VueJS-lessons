da991c76c107a304bbcb0b7e0ad067766381b3cb
Evgenii Popov <jekson87@me.com>
2020-01-29 16:36:57 +0300

    exercise 4 result

--- js/exercise4.js
+++ js/exercise4.js
@@ -2,10 +2,16 @@ new Vue({
     el: '#exercise',
     data: {
         switchClass: true,
-        startDemo: false,
+        startedDemo: false,
         timerId: null,
         italic: 'italic',
-        chosenClass: null
+        input1: null,
+        input2: null,
+        input3: null,
+        input4: null,
+        progressWidth: 0,
+        progressTimerId: null,
+        startedProgressBar: false
     },
     computed: {
         divClasses: function() {
@@ -13,18 +19,36 @@ new Vue({
                 highlight: this.switchClass,
                 shrink: !this.switchClass
             }
+        },
+        progressBar: function() {
+            return {
+                height: 24 + 'px',
+                width: this.progressWidth + '%',
+                background: 'grey'
+            }
         }
     },
     methods: {
         startEffect: function() {
-            this.startDemo = !this.startDemo;
-            if (this.startDemo === true)  {
+            this.startedDemo = !this.startedDemo;
+            if (this.startedDemo === true)  {
                 var vm = this;
                 this.timerId = setInterval(function() {
                                 vm.switchClass = !vm.switchClass;
                             }, 1000);
             } else {
                 return clearInterval(this.timerId);
+            }
+        },
+        startProgressBar: function() {
+            this.startedProgressBar = !this.startedProgressBar;
+            if (this.startedProgressBar === true)  {
+                var vm = this;
+                this.progressTimerId = setInterval(function() {
+                    vm.progressWidth = (vm.progressWidth + 1) % 30;
+                }, 100);
+            } else {
+                return clearInterval(this.progressTimerId);
             }
         }
     }
